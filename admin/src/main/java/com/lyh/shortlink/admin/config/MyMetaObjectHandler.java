ba3233b77ea28047892d50793ada55be07a64d0d
lyh-cf <1470511515@qq.com>
2024-01-24 20:54:03 +0800

    feature：通用持久层对象实体属性填充

---
+++ admin/src/main/java/com/lyh/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.lyh.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/*
+ *@title MyMetaObjectHandler
+ *@description
+ *@author LYH
+ *@version 1.0
+ *@create 2024/1/24 20:22
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
