ff4a896c4e6458f443f1b85d3b1dc365e3d006a3
lyh-cf <1470511515@qq.com>
2024-01-24 11:39:55 +0800

    feature：开发短链接规约

---
+++ admin/src/main/java/com/lyh/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,24 @@
+package com.lyh.shortlink.admin.common.convention.errorcode;
+
+/*
+ *@title IErrorCode
+ *@description
+ *@author LYH
+ *@version 1.0
+ *@create 2024/1/24 10:33
+ */
+/**
+ * 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
