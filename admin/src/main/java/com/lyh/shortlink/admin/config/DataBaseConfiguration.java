cbaeb2a568811aae690018280be4e55a13ea9db8
lyh-cf <1470511515@qq.com>
2024-02-04 21:22:35 +0800

    feature：配置MySQL数据库分页插件

---
+++ admin/src/main/java/com/lyh/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.lyh.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ *@title DataBaseConfiguration
+ *@description
+ *@author LYH
+ *@version 1.0
+ *@create 2024/2/4 21:19
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
