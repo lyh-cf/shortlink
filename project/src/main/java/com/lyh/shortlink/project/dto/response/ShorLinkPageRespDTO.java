430e0ba562336b0df7bc0427ea9110e897e73bd7
lyh-cf <1470511515@qq.com>
2024-02-05 15:08:20 +0800

    feature：分页查询短链接响应对象新增创建时间以及时间序列化

--- project/src/main/java/com/lyh/shortlink/project/dto/response/ShorLinkPageRespDTO.java
+++ project/src/main/java/com/lyh/shortlink/project/dto/response/ShorLinkPageRespDTO.java
@@ -1,5 +1,6 @@
 package com.lyh.shortlink.project.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -55,10 +56,16 @@ public class ShorLinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
      * 描述
      */
     private String describe;
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 }
