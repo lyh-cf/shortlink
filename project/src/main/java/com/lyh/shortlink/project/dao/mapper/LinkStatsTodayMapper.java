0da0f5920b881cbf5bd10ea2e9c42de77fb14d14
lyh-cf <1470511515@qq.com>
2024-02-27 19:45:51 +0800

    feature：开发短链接分页监控相关功能

---
+++ project/src/main/java/com/lyh/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,14 @@
+package com.lyh.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lyh.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/*
+ *@title LinkStatsTodayMapper
+ *@description 短链接今日统计持久层
+ *@author LYH
+ *@version 1.0
+ *@create 2024/2/27 19:14
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
--- project/src/test/java/com/lyh/shortlink/project/test/LinkTableShardingTest.java
+++ project/src/test/java/com/lyh/shortlink/project/test/LinkTableShardingTest.java
@@ -17,17 +17,20 @@ public class LinkTableShardingTest {
             "  `click_num` int NULL DEFAULT 0 COMMENT '点击量',\n" +
             "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分组标识',\n" +
             "  `favicon` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '网站图标',\n" +
-            "  `enable_status` tinyint(1) NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
+            "  `enable_status` tinyint(1) NULL DEFAULT NULL COMMENT '启用标识 0：启用 1：未启用',\n" +
             "  `created_type` tinyint(1) NULL DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
             "  `valid_date_type` tinyint(1) NULL DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
             "  `valid_date` datetime NULL DEFAULT NULL COMMENT '有效期',\n" +
             "  `describe` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '描述',\n" +
+            "  `total_pv` int NULL DEFAULT NULL COMMENT '历史PV',\n" +
+            "  `total_uv` int NULL DEFAULT NULL COMMENT '历史UV',\n" +
+            "  `total_uip` int NULL DEFAULT NULL COMMENT '历史UIP',\n" +
             "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`) USING BTREE,\n" +
-            "  UNIQUE INDEX `idx_unique_full_short_url`(`full_short_url` ASC) USING BTREE\n" +
-            ") ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;\n";
+            "  UNIQUE INDEX `idx_unique_full-short-url`(`full_short_url` ASC) USING BTREE\n" +
+            ") ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;";
     public static void main(String[] args) {
         for (int i=0;i<16;i++){
             System.out.printf((SQL)+"%n",i);
