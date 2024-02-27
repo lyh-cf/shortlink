39b55e26d1b0f3c5903211cee47d266498028d00
lyh-cf <1470511515@qq.com>
2024-02-27 19:03:45 +0800

    docs：补充访问日志监控开发注释

--- project/src/main/java/com/lyh/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
+++ project/src/main/java/com/lyh/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
@@ -269,6 +269,7 @@ public class ShortLinkStatsServiceImpl implements ShortLinkStatsService {
                 .toList();
         //获取用户信息是否新老访客
         List<Map<String, Object>> uvTypeList = linkAccessLogsMapper.selectUvTypeByUsers(
+                //将参数拆开，单独传，如果直接传 requestParam 会被mybatis进行分页转换
                 requestParam.getGid(),
                 requestParam.getFullShortUrl(),
                 requestParam.getStartDate(),
