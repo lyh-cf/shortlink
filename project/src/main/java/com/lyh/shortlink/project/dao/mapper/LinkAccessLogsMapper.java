407b47cdc4e42c250d13651e2a050879f35cac6c
lyh-cf <1470511515@qq.com>
2024-02-27 16:33:18 +0800

    refactor：将映射器接口类的注解SQL移至映射文件

--- project/src/main/java/com/lyh/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
+++ project/src/main/java/com/lyh/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -5,7 +5,6 @@ import com.lyh.shortlink.project.dao.entity.LinkAccessLogsDO;
 import com.lyh.shortlink.project.dao.entity.LinkAccessStatsDO;
 import com.lyh.shortlink.project.dto.request.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,45 +29,11 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
     /**
      * 根据短链接获取指定日期内新旧访客数据
      */
-    @Select("SELECT " +
-            "    SUM(old_user) AS oldUserCnt, " +
-            "    SUM(new_user) AS newUserCnt " +
-            "FROM ( " +
-            "    SELECT " +
-            "        CASE WHEN COUNT(DISTINCT DATE(create_time)) > 1 THEN 1 ELSE 0 END AS old_user, " +
-            "        CASE WHEN COUNT(DISTINCT DATE(create_time)) = 1 AND MAX(create_time) >= #{param.startDate} AND MAX(create_time) <= #{param.endDate} THEN 1 ELSE 0 END AS new_user " +
-            "    FROM " +
-            "        t_link_access_logs " +
-            "    WHERE " +
-            "        full_short_url = #{param.fullShortUrl} " +
-            "        AND gid = #{param.gid} " +
-            "    GROUP BY " +
-            "        user " +
-            ") AS user_counts;")
     HashMap<String, Object> findUvTypeCntByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
     /**
      * 获取用户信息是否新老访客
      */
-    @Select("<script> " +
-            "SELECT " +
-            "    user, " +
-            "    CASE " +
-            "        WHEN MIN(create_time) BETWEEN #{startDate} AND #{endDate} THEN '新访客' " +
-            "        ELSE '老访客' " +
-            "    END AS uvType " +
-            "FROM " +
-            "    t_link_access_logs " +
-            "WHERE " +
-            "    full_short_url = #{fullShortUrl} " +
-            "    AND gid = #{gid} " +
-            "    AND user IN " +
-            "    <foreach item='item' index='index' collection='userAccessLogsList' open='(' separator=',' close=')'> " +
-            "        #{item} " +
-            "    </foreach> " +
-            "GROUP BY " +
-            "    user;" +
-            "    </script>"
-    )
+    @SuppressWarnings("MybatisXMapperMethodInspection")
     List<Map<String, Object>> selectUvTypeByUsers(
             @Param("gid") String gid,
             @Param("fullShortUrl") String fullShortUrl,
