e11526596bd8b7303d44a1adcca0807cc7d48b64
lyh-cf <1470511515@qq.com>
2024-03-01 14:21:05 +0800

    feature: 短链接后管通过流量风控保障系统稳定性

--- admin/src/main/java/com/lyh/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/lyh/shortlink/admin/config/UserConfiguration.java
@@ -8,6 +8,7 @@ package com.lyh.shortlink.admin.config;
  *@create 2024/1/26 0:39
  */
 
+import com.lyh.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.lyh.shortlink.admin.common.biz.user.UserTransmitFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -30,8 +31,24 @@ public class UserConfiguration {
         registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
         //设置过滤器要起作用的路径
         registration.addUrlPatterns("/*");
-        //设置过滤器执行的顺序，如果有同样的过滤器，先执行order最大的，然后依次执行
+        //设置过滤器执行的顺序，先执行order最小的
         registration.setOrder(0);
+        return registration;
+    }
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    //条件注解
+//    @ConditionalOnProperty(name = "shortlink.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        //这个是自己创建的一个过滤器类，并没有通过spring管理，不能通过spring注入
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        //设置过滤器执行的顺序，先执行order最小的
+        registration.setOrder(10);
         return registration;
     }
 }
---
+++ admin/src/main/resources/lua/user_flow_risk_control.lua
@@ -0,0 +1,29 @@
+-- 设置用户访问频率限制的参数
+local username = KEYS[1]
+local timeWindow = tonumber(ARGV[1]) -- 时间窗口，单位：秒
+
+-- 构造 Redis 中存储用户访问次数的键名
+local accessKey = "shortlink:user-flow-risk-control:" .. username
+
+local result = redis.call("exists",accessKey)
+-- 存在该 key
+if( result == 1 )
+then
+    -- 原子递增访问次数，并获取递增后的值
+    local currentAccessCount = redis.call("INCR", accessKey)
+    -- 返回当前访问次数
+    return currentAccessCount
+-- 不存在该 key
+else
+    -- 原子递增访问次数，并获取递增后的值
+    local currentAccessCount = redis.call("INCR", accessKey)
+    -- 设置键的过期时间
+    redis.call("EXPIRE", accessKey, timeWindow)
+    -- 返回当前访问次数
+    return currentAccessCount
+end
+
+
+
+
+
